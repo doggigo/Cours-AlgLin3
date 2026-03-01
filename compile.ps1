18af467fc50a6e09bd47d666cc5d81f103d452d7
Robin DAVID--MULLER <140105689+doggigo@users.noreply.github.com>
2026-03-01 23:15:04 +0100

    Updated

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,5 @@
 typst compile "./cours.typ" "./cours-only-thms.pdf" --input only-thms=true 
 typst compile "./cours.typ" "./cours.pdf"
-typst compile "./cours.typ" "./cours-only-thms-bw.pdf" --input only-thms=true theme=bw
+typst compile "./cours.typ" "./cours-only-thms-bw.pdf" --input only-thms=true --input theme=bw
 typst compile "./cours.typ" "./cours-bw.pdf" --input theme=bw
 echo "Documents compilés avec succès"
